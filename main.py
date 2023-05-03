2f7263f7af67d53f37f5f380c4f18937b9d5b441
Fakemrx <vkachan2003@gmail.com>
2023-05-03 13:53:14 +0300

    MongoDB setup

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+"""Main file of all project"""
 from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+config = dotenv_values(".env")
 
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello Develop"}
+@app.on_event("startup")
+def startup_db_client():
+    """Launches MongoDB after an app was started"""
+    app.mongodb_client = MongoClient(config["DATABASE_URL"])
+    app.database = app.mongodb_client[config["MONGO_INITDB_DATABASE"]]
+    print("Connected to the MongoDB database!")
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    """Shuts down DB after an app was stopped."""
+    app.mongodb_client.close()
+    print("MongoDB database connection were closed.")
